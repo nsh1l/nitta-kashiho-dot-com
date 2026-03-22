cb442c27c7601174545d7cc907483017b1699a59
nsh1l <nsh1l@outlook.com>
2026-03-22 17:32:12 +0900

    add/ ビルド情報の出力

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -27,6 +27,18 @@ export default function (eleventyConfig) {
     () => `${new Date().getFullYear()}`,
   );
 
+  eleventyConfig.addShortcode("runtime", () => {
+    const now = new Date();
+    return now.toLocaleString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+  });
+
   eleventyConfig.setLibrary(
     "md",
     markdown({
